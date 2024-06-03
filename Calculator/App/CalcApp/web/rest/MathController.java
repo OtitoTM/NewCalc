551dad3348525ab33c1291910ac7beb751584100
OtitoTM <129734408+OtitoTM@users.noreply.github.com>
2024-06-03 12:32:24 +0300

    Add files via upload

---
+++ Calculator/App/CalcApp/web/rest/MathController.java
@@ -0,0 +1,35 @@
+package Calculator.App.CalcApp.web.rest;
+
+
+import Calculator.App.CalcApp.Service.DTO.MathRequestDTO;
+import Calculator.App.CalcApp.Service.MathService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("CalcApp/application")
+public class MathController {
+    MathService mathService = new MathService();
+
+    @PostMapping("/add")
+    public double add(@RequestBody MathRequestDTO mathRequestDTO) {
+        return mathService.add(mathRequestDTO.getNumber1(), mathRequestDTO.getNumber2());
+    }
+
+    @PostMapping("/subtract")
+    public double subtract(@RequestBody MathRequestDTO mathRequestDTO) {
+        return mathService.subtract(mathRequestDTO.getNumber1(), mathRequestDTO.getNumber2());
+    }
+
+    @PostMapping("/multiply")
+    public double multiply(@RequestBody MathRequestDTO mathRequestDTO) {
+        return mathService.multiply(mathRequestDTO.getNumber1(), mathRequestDTO.getNumber2());
+    }
+
+    @PostMapping("/divide")
+    public double divide(@RequestBody MathRequestDTO mathRequestDTO) {
+        return mathService.divide(mathRequestDTO.getNumber1(), mathRequestDTO.getNumber2());
+    }
+}
